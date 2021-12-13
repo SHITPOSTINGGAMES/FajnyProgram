e8a3cdd1f5d015f0666d54da8787e62cb72307fb
SHITPOSTINGGAMES <94223763+SHITPOSTINGGAMES@users.noreply.github.com>
2021-12-13 08:51:42 +0100

    Add files via upload

---
+++ Super programik!!!!1111/1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char** argv) {
+	int doj,zmar;
+	cout << "Ile dzisiaj razy zorzechowales brahu?" << "\n";
+	cin >> doj;
+	zmar = doj*100000000;
+	cout << "WTF BRAHU ZMARNOWALES " << zmar << " PLEMNIKUF";
+	
+	
+	return 0;
+}
---
+++ Super programik!!!!1111/readme.txt
@@ -0,0 +1,9 @@
+----------------------------------------------------------------------------
+Program jest olpen sors
+----------------------------------------------------------------------------
+Altor : Gadzbereg
+----------------------------------------------------------------------------
+Program oplicza ile plemnikuf dzisjai zmarnofales :DDDDDDDDDDDDDDDDDDDD
+
+Licencyja
+GNU GPL
